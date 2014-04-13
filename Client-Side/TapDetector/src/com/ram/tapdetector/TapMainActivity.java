36a4fabe29eef5e73aa0ecd7fac415d5f307674c
pyromanic <akashns@hotmail.com>
2014-04-12 20:05:18 -0400

    TapApp
    Change

---
+++ Client-Side/TapDetector/src/com/ram/tapdetector/TapMainActivity.java
@@ -0,0 +1,31 @@
+package com.ram.tapdetector;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.MotionEvent;
+
+/**
+ * Created by Akash on 4/12/14.
+ */
+
+
+public class TapMainActivity extends Activity {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        int x = (int)event.getX();
+        int y = (int)event.getY();
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+            case MotionEvent.ACTION_UP:
+        }
+        return false;
+    }
+
+
+
+}
