6361c49421dc906ffd11529e357eff3c32c7de7f
nishilshah17 <nishil.shah17@gmail.com>
2014-04-12 22:14:30 -0400

    callmanager

---
+++ Client-Side/TapDetector/src/com/ram/tapdetector/CallManager.java
@@ -0,0 +1,24 @@
+public class CallManager extends Broadcast Receiver {
+
+    public abstract void onReceive(Context context, Intent intent) {
+        
+        TelephonyManager tm = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+           try {
+             Class c = Class.forName(tm.getClass().getName());
+             Method m = c.getDeclaredMethod("getITelephony");
+             m.setAccessible(true);
+             telephonyService = (ITelephony) m.invoke(tm);
+             Bundle bundle = intent.getExtras();
+             String phoneNumber = bundle.getString("incoming_number");
+             Log.d("INCOMING", phoneNumber);
+             if ((phoneNumber != null)) { 
+                telephonyService.endCall();
+                Log.d("HANG UP", phoneNumber);
+             }
+        
+           } catch (Exception e) {
+             e.printStackTrace();
+        }
+    }
+    
+}
