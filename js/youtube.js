367af7fdd6b435a56d13704b065e8fa109b9dfb7
nishilshah17 <nishil.shah17@gmail.com>
2014-05-24 16:12:58 -0400

    fdsa

--- js/youtube.js
+++ js/youtube.js
@@ -4,19 +4,6 @@ var vidURL;
 var results;
 //http://www.youtube.com/v/VIDEO_ID?version=3&enablejsapi=1
 
-/*function searchVideos() {
-  var q = search;
-  var request = gapi.client.youtube.search.list({
-    q: q,
-    part: 'snippet'
-  });
-    request.execute(function(response) {
-        var str = JSON.stringify(response.result);
-        results = str;
-  });
-    alert(results);
-}*/
-
 function searchVideos() {
 
     gapi.client.load('youtube', 'v3', function() {
@@ -32,10 +19,5 @@ function searchVideos() {
         
         var str = JSON.stringify(request);
         alert(str);
-
-        /*request.execute(function(resp) {
-          document.getElementById('vid').value = resp.items[0].id.videoId;
-          console.log('saved video id successfully');
-        });
       });
 }
