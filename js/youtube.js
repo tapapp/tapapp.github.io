4e754fbc3d978fbc54dce36cc593e3136b8d6da7
nishilshah17 <nishil.shah17@gmail.com>
2014-05-24 18:31:35 -0400

    fdsa

--- js/youtube.js
+++ js/youtube.js
@@ -1,8 +1,3 @@
-var script = document.createElement('script');
-script.src = 'js/jquery.js';
-script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
-
 var search;
 var category = "music";
 var vidURL;
