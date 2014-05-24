56ca34362f5e04c98b760caf19b3398e0ad49758
nishilshah17 <nishil.shah17@gmail.com>
2014-05-24 16:12:02 -0400

    fdsa

--- js/youtube.js
+++ js/youtube.js
@@ -4,7 +4,7 @@ var vidURL;
 var results;
 //http://www.youtube.com/v/VIDEO_ID?version=3&enablejsapi=1
 
-function searchVideos() {
+/*function searchVideos() {
   var q = search;
   var request = gapi.client.youtube.search.list({
     q: q,
@@ -15,4 +15,27 @@ function searchVideos() {
         results = str;
   });
     alert(results);
+}*/
+
+function searchVideos() {
+
+    gapi.client.load('youtube', 'v3', function() {
+        console.log('youtube API loaded...');
+        // Step 5: Assemble the API request
+        var qVar = "Kanye West Amazing";
+        // changed. added: type
+        var request = gapi.client.youtube.search.list({
+            type: 'video',
+            part: 'snippt',
+            q: qVar
+        });
+        
+        var str = JSON.stringify(request);
+        alert(str);
+
+        /*request.execute(function(resp) {
+          document.getElementById('vid').value = resp.items[0].id.videoId;
+          console.log('saved video id successfully');
+        });
+      });
 }
