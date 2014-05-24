ebb0c95f6a36e46b8a07519be0967f7bfc6428c2
nishilshah17 <nishil.shah17@gmail.com>
2014-05-24 18:01:39 -0400

    fda

--- js/youtube.js
+++ js/youtube.js
@@ -61,10 +61,10 @@ function setWorkout() {
 }
 
 function parseResults() {
-    for (int i = 0; i<results.length; i++)
+  /*  for (int i = 0; i<results.length; i++)
     {
         
-    }
+    }*/
     
     
 }
