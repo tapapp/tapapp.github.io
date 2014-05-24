f1900a52ac1251e83f5719dd52950497b97a1428
nishilshah17 <nishil.shah17@gmail.com>
2014-05-24 19:17:54 -0400

    fdsa

--- js/youtube.js
+++ js/youtube.js
@@ -70,12 +70,12 @@ function setSearch() {
 
 function parseResults() {
     alert(results.items);
-    var newResults = jQuery.parseJSON(results.items);
+    /*var newResults = jQuery.parseJSON(results.items);
     for(int i=0; i< newResults.length; i++)
     {
         alert(newResults.id);
-    }
-    /*$.each(results.items, function(item) {
-        alert(item.id.videoId);
-    }); */  
+    }*/
+    $.each(results.items, function(item) {
+        alert(item.id);
+    });  
 }
