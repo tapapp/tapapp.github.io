71a00116de738b23b483218bbaed7d6736662661
nishilshah17 <nishil.shah17@gmail.com>
2014-05-25 03:17:08 -0400

    fda

--- js/youtube.js
+++ js/youtube.js
@@ -123,8 +123,9 @@ function parseResults() {
     $.each(results.items, function(item) {
         if(results.items[item].id.videoId != null && results.items[item].id.videoId != undefined)
         {
-            
-            videoIDs.push(results.items[item].id.videoId);
+            if(removedVideos.indexOf(results.items[item].id.videoId) == -1) {
+                videoIDs.push(results.items[item].id.videoId);
+            }
             
         }
     });  
