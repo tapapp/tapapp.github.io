c444fb9fd24c2e7f342ecb9b2b1255c7d956465b
nishilshah17 <nishil.shah17@gmail.com>
2014-05-24 22:18:51 -0400

    fdsa

--- js/youtube.js
+++ js/youtube.js
@@ -105,7 +105,7 @@ function embedVideo() {
     currentVideo = videoIDs[currentIndex];
     
     $('#videoplayer').append( "<iframe width='560' height='310' src='http://www.youtube.com/embed/"+videoIDs[0]+"?&fs=0&controls=0&autohide=1&color=white&autoplay=1&version=3&enablejsapi=1' frameborder='0' ></iframe>'");
-                        
+    console.log("<iframe width='560' height='310' src='http://www.youtube.com/embed/"+videoIDs[0]+"?&fs=0&controls=0&autohide=1&color=white&autoplay=1&version=3&enablejsapi=1' frameborder='0' ></iframe>'");
 }
 
 function pause() {
